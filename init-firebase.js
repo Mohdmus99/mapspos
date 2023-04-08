a82dddb5af011853cf3ba798991a2e5f41646db5
Mohammed Mustafa <mohammedmustafa@Mohammeds-MacBook-Pro.local>
2023-04-09 00:03:09 +0300

    s

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDDWmUpRMrLBS5UKAyllEC72YKpb1BEu0w",
+    authDomain: "titanium-4cd25.firebaseapp.com",
+    projectId: "titanium-4cd25",
+    storageBucket: "titanium-4cd25.appspot.com",
+    messagingSenderId: "867812196684",
+    appId: "1:867812196684:web:415636c109a2bb1743cfe9",
+    measurementId: "G-LWE6F36XPT"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
